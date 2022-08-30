acbb2cf19ade9fa96d6ca0a5e031ca48d3d9850f
Diego Sept <septdiego@gmail.com>
2022-08-29 23:13:16 -0300

    AddToCart component semi-fixed

--- src/AddToCart/AddToCart.jsx
+++ src/AddToCart/AddToCart.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Container, Form } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import "./AddToCart.css"
@@ -7,17 +7,17 @@ import { BsCart4, BsBagPlusFill, BsBagDashFill } from 'react-icons/bs';
 
 function AddToCart({ product, ...props }) {
 
-    var contador = 0;
+    let [contador, setContador] = useState(0);
 
     const substractItem = () => {
         if (contador > 0) {
-            contador--;
+            setContador(contador--);
         }
     }
 
     function addItem() {
         if (contador < product.available_quantity) {
-            contador++;
+            setContador(contador++);
             console.log(contador);
         }
     }
@@ -52,13 +52,15 @@ function AddToCart({ product, ...props }) {
         <>
             <Container className="d-flex justify-content-between">
                 <Container className="d-flex justify-content-evenly">
-                    <Button variant={variantBtnSubstract()} onClick={() => {substractItem()}} disabled={disableBtnSubstract()}>
+                    <Button variant={(Number(contador) === 0) ? 'light' : 'danger'} onClick={() => {substractItem()}}
+                        disabled={(Number(contador) === Number(0)) ? true : false}>
                         <BsBagDashFill />
                     </Button>
                     <Form className="input-styles">
-                        <Form.Control type="number" placeholder={contador}></Form.Control>
+                        <Form.Control type="number" value={Number(contador)} readOnly={true}></Form.Control>
                     </Form>
-                    <Button variant={variantBtnAdd()} onClick={() => {addItem()}} disabled={disableBtnAdd()}>
+                    <Button variant={(Number(contador) === Number(product.available_quantity)) ? 'light' : 'success'} onClick={() => {addItem()}} 
+                        disabled={(Number(contador) === Number(product.available_quantity)) ? true : false}>
                         <BsBagPlusFill />
                     </Button>
                 </Container>
